403335bad378398a5d1abceda01427d7508b247f
Gal Shnapp <gal.shnapp@gmail.com>
2018-06-19 19:21:22 +0300

    login now redirects

--- www/login.js
+++ www/login.js
@@ -9,4 +9,14 @@ $(document).ready(function(){
     $("a").click(function(){
         $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
     });
+    $("#createButton").click(function(){
+        alert('create');
+    });
+
+    $("#loginButton").click(function() {
+        url = "http://127.0.0.1:8081/ideas.html";
+        $(location).attr("href", url);
+        return false;
+    }); 
 });
+
