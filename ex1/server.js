f6eb220ea9a7e85649af7cb6c921818072f1a9af
Gal Shnapp <gal.shnapp@gmail.com>
2018-06-19 10:36:24 +0300

    alrighty

--- ex1/server.js
+++ ex1/server.js
@@ -19,57 +19,39 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post('/ideaup', function(req,res){
     // /idea/<id> (POST) - update an idea (string)  by it’s id
-    console.log("post");
-    console.log(ideas);
     var data = req.body;
     for(var key in data){
         ideas[key] = data[key];
     }
-    console.log(ideas);
-});
-
-
-app.get('/roro', function(req,res){
-    console.log("post");
-    console.log(ideas);
-    res.send('this is my testing roror is the king ');
-
 });
 
 
 app.delete('/idead', function(req,res){
     // /idea/<id> (DELETE) - delete an idea by it’s id (returns 0 if success, 1 otherwise)
-    console.log("delete");
-    console.log(ideas);
     var data = req.body;
     for(var key in data){
         delete ideas[key];
     }
-    console.log(ideas);   
 });
 
 
 app.put('/ideapu', function(req,res) {
     // /idea (PUT) - add new idea ( idea is just a string) returns the idead’s id
-    console.log("put");
-    console.log(ideas);
     var data = req.body;
     for(var key in data){
         ideas[key] = data[key];
     }
-    console.log(ideas);
 });
 
 app.get('/ideas', function (req, res) {
     // /ideas (GET) - returns all the ideas as an object whereas id(number) -> idea(string)
     res.setHeader('Content-Type', 'application/json');
     res.send(JSON.stringify(ideas)); 
-    console.log("get");
-})
+});
 
 var server = app.listen(8081, function () {
    var host = server.address().address;
    var port = server.address().port;
    
-   console.log("Example app listening at http://%s:%s", host, port)
+   console.log("Example app listening at http://%s:%s", host, port);
 });
