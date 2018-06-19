4b7488959b34cc5bd51a00db21817eeafadf7912
Gal Shnapp <gal.shnapp@gmail.com>
2018-06-19 16:52:31 +0300

    login-page-anim is proper

--- www/login.js
+++ www/login.js
@@ -1,5 +1,12 @@
+console.log('a');
 $('.message a').click(function(){
     $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
- });
+    console.log('a');
+});
+console.log('b');
 
- 
+$(document).ready(function(){
+    $("a").click(function(){
+        $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
+    });
+});
