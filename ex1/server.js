ab0ec43d86d0d8ef60c7b14cd9b13440d2590253
Gal Shnapp <gal.shnapp@gmail.com>
2018-06-29 20:18:40 +0300

    ex2 submitted

--- ex1/server.js
+++ ex1/server.js
@@ -1,3 +1,4 @@
+// @ts-check
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
@@ -11,22 +12,27 @@ var path = require('path');
 
 
 // parse the body (populates req.body)
-app.use(bodyParser.json()); 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 // parse cookies (populates req.cookies)
 app.use(cookieParser());
 //  if session is in progress, update the cookies
-app.use(function(req,res,next){
-    if(Object.keys(req.cookies).length > 0){
-        res.cookie('username', req.cookies.username, {maxAge : 1800000});
+app.use(function (req, res, next) {
+    if (Object.keys(req.cookies).length > 0) {
+        res.cookie('username', req.cookies.username, {
+            maxAge: 1800000
+        });
     }
     next();
 });
 // serves public content
 app.use(express.static('www'));
 // error drain
-app.use(function(err,req,res,next){
-    if(err){
+// @ts-ignore
+app.use(function (err, req, res, next) {
+    if (err) {
         res.status(500);
         res.end();
         console.error(err);
@@ -39,110 +45,119 @@ app.use(function(err,req,res,next){
 });
 
 // register endpoint
-app.post('/users/register', function(req,res){
+app.post('/users/register', function (req, res) {
     console.log('create req');
     var username = req.body.username;
     var password = req.body.pass;
+    // @ts-ignore
     var DB = JSON.parse(fs.readFileSync('DB.txt'));
-    if(!DB.hasOwnProperty(username)){
+    if (!DB.hasOwnProperty(username)) {
         console.log('undef');
-        DB[username]={
-            pw : password,
-            ideas:{
-                1 : 'get friends to use this app',
-                2 : 'contribute to society'
-            }   
+        DB[username] = {
+            pw: password,
+            ideas: {
+                1: 'get friends to use this app',
+                2: 'contribute to society'
+            }
         };
-        fs.writeFileSync('DB.txt',JSON.stringify(DB));
-        res.cookie('username', username, {maxAge : 1000*60*30});
+        fs.writeFileSync('DB.txt', JSON.stringify(DB));
+        res.cookie('username', username, {
+            maxAge: 1000 * 60 * 30
+        });
         res.status(200);
-        res.sendFile(path.join(__dirname,"priv","ideas.html")); 
-    } else{
+        res.sendFile(path.join(__dirname, "priv", "ideas.html"));
+    } else {
         console.log('def');
         res.redirect("/login.html");
     }
-    
+
 });
 
 // login end-point
-app.post('/users/login',function(req,res){
+app.post('/users/login', function (req, res) {
     console.log("login request");
     var username = req.body.username;
     var password = req.body.pass;
+    // @ts-ignore
     var DB = JSON.parse(fs.readFileSync('DB.txt'));
-    if(typeof DB[username] !== undefined){
-        var usr=DB[username];
-        if(password == usr.pw){
+    if (typeof DB[username] !== undefined) {
+        var usr = DB[username];
+        if (password == usr.pw) {
             console.log("pw matches");
-            res.cookie('username', username, {maxAge : 1000*60*30});
+            res.cookie('username', username, {
+                maxAge: 1000 * 60 * 30
+            });
             res.status(200);
-            res.sendFile(path.join(__dirname,"priv","ideas.html"));
+            res.sendFile(path.join(__dirname, "priv", "ideas.html"));
         } else {
             console.log("pw doesn't match");
             res.status(401);
             res.redirect('/login.html');
         }
-    }else{
+    } else {
         console.log("no such user");
         res.status(401);
         res.redirect('/login.html');
     }
-    
-    
+
+
 });
 
 
 
 app.get('/', function (req, res) {
-    
-    if(Object.keys(req.cookies).length > 0){   
+
+    if (Object.keys(req.cookies).length > 0) {
         res.status(200);
-        res.sendFile(path.join(__dirname,"priv","ideas.html"));
-    }else{
+        res.sendFile(path.join(__dirname, "priv", "ideas.html"));
+    } else {
         res.redirect('/login.html');
     }
-    
+
 });
 
-app.post('/ideaup', function(req,res){
+app.post('/ideaup', function (req, res) {
     // /idea/<id> (POST) - update an idea (string)  by it’s id
+    // @ts-ignore
     var DB = JSON.parse(fs.readFileSync('DB.txt'));
     var ideas = DB[req.cookies.username].ideas;
     var data = req.body;
-    for(var key in data){
+    for (var key in data) {
         ideas[key] = data[key];
     }
-    fs.writeFileSync('DB.txt',JSON.stringify(DB));
+    fs.writeFileSync('DB.txt', JSON.stringify(DB));
     res.status(200);
     res.send();
 
 });
 
 
-app.delete('/idead', function(req,res){
+app.delete('/idead', function (req, res) {
     // /idea/<id> (DELETE) - delete an idea by it’s id (returns 0 if success, 1 otherwise)
+    // @ts-ignore
     var DB = JSON.parse(fs.readFileSync('DB.txt'));
     var ideas = DB[req.cookies.username].ideas;
     var data = req.body;
-    for(var key in data){
+    for (var key in data) {
         delete ideas[key];
     }
-    fs.writeFileSync('DB.txt',JSON.stringify(DB));
+    fs.writeFileSync('DB.txt', JSON.stringify(DB));
     res.status(200);
     res.send();
 
 });
 
 
-app.put('/ideapu', function(req,res) {
+app.put('/ideapu', function (req, res) {
     // /idea (PUT) - add new idea ( idea is just a string) returns the idead’s id
+    // @ts-ignore
     var DB = JSON.parse(fs.readFileSync('DB.txt'));
     var ideas = DB[req.cookies.username].ideas;
     var data = req.body;
-    for(var key in data){
+    for (var key in data) {
         ideas[key] = data[key];
     }
-    fs.writeFileSync('DB.txt',JSON.stringify(DB));
+    fs.writeFileSync('DB.txt', JSON.stringify(DB));
     res.status(200);
     res.send();
 
@@ -150,15 +165,18 @@ app.put('/ideapu', function(req,res) {
 
 app.get('/ideas', function (req, res) {
     // /ideas (GET) - returns all the ideas as an object whereas id(number) -> idea(string)
+    // @ts-ignore
     var DB = JSON.parse(fs.readFileSync('DB.txt'));
     var ideas = DB[req.cookies.username].ideas;
     res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify(ideas)); 
+    res.send(JSON.stringify(ideas));
 });
 
 var server = app.listen(8081, function () {
-   var host = server.address().address;
-   var port = server.address().port;
-   
-   console.log("Example app listening at http://%s:%s", host, port);
+    // @ts-ignore
+    var host = server.address().address;
+    // @ts-ignore
+    var port = server.address().port;
+
+    console.log("Example app listening at http://%s:%s", host, port);
 });
--- www/ideas.js
+++ www/ideas.js
@@ -1,4 +1,4 @@
-function alertMe(){
+function alertMe() {
     alert('Dude, take some deep breaths..');
 }
 
@@ -6,120 +6,120 @@ var list;
 
 // Example POST method implementation:
 
-function updateIdea(id,value){
-    
-    var data= {};
-    data[id]=value;
+function updateIdea(id, value) {
+
+    var data = {};
+    data[id] = value;
     postData(data)
-    .then(data => console.log(data)) // JSON from `response.json()` call
-    .catch(error => console.error(error))
+        .then(data => console.log(data)) // JSON from `response.json()` call
+        .catch(error => console.error(error))
 }
 
-function addIdea(id, value){
-  
-    var data= {};
-    data[id]=value;
+function addIdea(id, value) {
+
+    var data = {};
+    data[id] = value;
     putData(data)
-    .then(data => console.log(data)) // JSON from `response.json()` call
-    .catch(error => console.error(error))
+        .then(data => console.log(data)) // JSON from `response.json()` call
+        .catch(error => console.error(error))
 }
 
-function removeIdea(id){
-   
-    var data= {};
-    data[id]=id;
+function removeIdea(id) {
+
+    var data = {};
+    data[id] = id;
     deleteData(data)
-    .then(data => console.log(data)) // JSON from `response.json()` call
-    .catch(error => console.error(error))
+        .then(data => console.log(data)) // JSON from `response.json()` call
+        .catch(error => console.error(error))
 }
 
 function postData(data) {
-  // Default options are marked with *
-  return fetch("http://127.0.0.1:8081/ideaup", {
-    body: JSON.stringify(data), // must match 'Content-Type' header
-    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-    credentials: 'same-origin', // include, same-origin, *omit
-    headers: {
-      'content-type': 'application/json'
-    },
-    method: 'POST', // *GET, POST, PUT, DELETE, etc.
-    mode: 'cors', // no-cors, cors, *same-origin
-    redirect: 'follow', // manual, *follow, error
-    referrer: 'no-referrer', // *client, no-referrer
-  });
-  
+    // Default options are marked with *
+    return fetch("http://127.0.0.1:8081/ideaup", {
+        body: JSON.stringify(data), // must match 'Content-Type' header
+        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: 'same-origin', // include, same-origin, *omit
+        headers: {
+            'content-type': 'application/json'
+        },
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        mode: 'cors', // no-cors, cors, *same-origin
+        redirect: 'follow', // manual, *follow, error
+        referrer: 'no-referrer', // *client, no-referrer
+    });
+
 }
 
-function putData(data){
+function putData(data) {
     return fetch("http://127.0.0.1:8081/ideapu", {
         body: JSON.stringify(data), // must match 'Content-Type' header
         cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
         credentials: 'same-origin', // include, same-origin, *omit
         headers: {
-          'content-type': 'application/json'
+            'content-type': 'application/json'
         },
         method: 'PUT', // *GET, POST, PUT, DELETE, etc.
         mode: 'cors', // no-cors, cors, *same-origin
         redirect: 'follow', // manual, *follow, error
         referrer: 'no-referrer', // *client, no-referrer
-      });
+    });
 
 }
 
-function deleteData(data){
+function deleteData(data) {
 
     return fetch("http://127.0.0.1:8081/idead", {
         body: JSON.stringify(data), // must match 'Content-Type' header
         cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
         credentials: 'same-origin', // include, same-origin, *omit
         headers: {
-          'content-type': 'application/json'
+            'content-type': 'application/json'
         },
         method: 'DELETE', // *GET, POST, PUT, DELETE, etc.
         mode: 'cors', // no-cors, cors, *same-origin
         redirect: 'follow', // manual, *follow, error
         referrer: 'no-referrer', // *client, no-referrer
-      });
-
-}
-
-function loadList(){
-    
-    return fetch("http://127.0.0.1:8081/ideas", {
-    body: {}, // must match 'Content-Type' header
-    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-    credentials: 'same-origin', // include, same-origin, *omit
-    headers: {
-      'content-type': 'application/json'
-    },
-    method: 'GET', // *GET, POST, PUT, DELETE, etc.
-    mode: 'cors', // no-cors, cors, *same-origin
-    redirect: 'follow', // manual, *follow, error
-    referrer: 'no-referrer', // *client, no-referrer
-  })
-    .then(function(response) {
-
-        return response.json();
-    })
-    .then(function(obj) {
-        for (var key in obj) {
-            var idea = ideaFactory(key, obj[key]);
-            list.insertBefore(idea, list.childNodes[0]);    
-        }
-
     });
+
 }
 
-function ideaFactory(key, value){
+function loadList() {
+
+    return fetch("http://127.0.0.1:8081/ideas", {
+            body: {}, // must match 'Content-Type' header
+            cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+            credentials: 'same-origin', // include, same-origin, *omit
+            headers: {
+                'content-type': 'application/json'
+            },
+            method: 'GET', // *GET, POST, PUT, DELETE, etc.
+            mode: 'cors', // no-cors, cors, *same-origin
+            redirect: 'follow', // manual, *follow, error
+            referrer: 'no-referrer', // *client, no-referrer
+        })
+        .then(function (response) {
+
+            return response.json();
+        })
+        .then(function (obj) {
+            for (var key in obj) {
+                var idea = ideaFactory(key, obj[key]);
+                list.insertBefore(idea, list.childNodes[0]);
+            }
+
+        });
+}
+
+function ideaFactory(key, value) {
     var ideaBox = document.createElement("DIV");
     var idea = document.createElement("P");
 
     var editBtn = document.createElement("BUTTON");
-    editBtn.setAttribute("onclick", "toEditButton("+ key + ")");
+    editBtn.setAttribute("onclick", "toEditButton(" + key + ")");
     editBtn.innerHTML = "Edit";
 
     var rmvBtn = document.createElement("BUTTON");
-    rmvBtn.setAttribute("onclick", "rmvIdea("+ key + ")");
+    rmvBtn.setAttribute("onclick", "rmvIdea(" + key + ")");
     rmvBtn.innerHTML = "Remove";
 
     idea.innerHTML = value;
@@ -128,23 +128,23 @@ function ideaFactory(key, value){
     ideaBox.appendChild(idea);
     ideaBox.appendChild(editBtn);
     ideaBox.appendChild(rmvBtn);
-    
-    
+
+
 
     return ideaBox;
 
 }
 
-function toEditButton(id){
+function toEditButton(id) {
 
     var origRef = document.getElementById(id);
     var editable = document.createElement("DIV");
     var textArea = document.createElement("TEXTAREA");
     var addBlk = document.createElement("DIV");
     var saveBtn = document.createElement("BUTTON");
-    
-    editable.setAttribute("id",id);
-    saveBtn.setAttribute("onclick", "save("+ id + ")");
+
+    editable.setAttribute("id", id);
+    saveBtn.setAttribute("onclick", "save(" + id + ")");
     saveBtn.innerHTML = "Save";
     editable.setAttribute("class", "container darker");
     addBlk.setAttribute("class", "addBlock");
@@ -153,27 +153,27 @@ function toEditButton(id){
     editable.appendChild(textArea);
     editable.appendChild(addBlk);
 
-    list.replaceChild(editable,origRef);
+    list.replaceChild(editable, origRef);
 
 }
 
-function save(id){
-    
+function save(id) {
+
     var origRef = document.getElementById(id);
     var val = origRef.childNodes[0].value + "";
     var ideaBox = ideaFactory(id, val);
-    list.replaceChild(ideaBox , origRef);
+    list.replaceChild(ideaBox, origRef);
     updateIdea(id, val);
 }
 
-function rmvIdea(id){
-    
+function rmvIdea(id) {
+
     list.removeChild(document.getElementById(id));
-    console.log("button " + id + " wants a removal" );
+    console.log("button " + id + " wants a removal");
     removeIdea(id);
 }
 
-function newIdea(){
+function newIdea() {
     var id = Math.random();
     var val = document.getElementById("addTxt").value;
     var newIdea = ideaFactory(id, val);
@@ -182,7 +182,7 @@ function newIdea(){
     addIdea(id, val);
 }
 
-window.onload = function(){
+window.onload = function () {
     list = document.getElementById("lCon");
     loadList();
 };
